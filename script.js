6cfae00dda7e49f37273f3bab4a53af13891a154
johny2433 <johny.codmw3@gmail.com>
2020-11-17 21:32:52 +0200

    generate random equations, also wrong/rightequations creation functions

--- script.js
+++ script.js
@@ -32,6 +32,67 @@ const wrongFormat = [];
 
 // Scroll
 
+// Get Random Number up to a max number
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Create Correct/Incorrect Random Equations
+function createEquations() {
+  // Randomly choose how many correct equations there should be
+  const correctEquations = getRandomInt(questionAmount);
+  console.log("correct: ", correctEquations);
+  // Set amount of wrong equations
+  const wrongEquations = questionAmount - correctEquations;
+  console.log("wrong: ", wrongEquations);
+  // Loop through, multiply random numbers up to 9, push to array
+  for (let i = 0; i < correctEquations; i++) {
+    firstNumber = getRandomInt(9);
+    secondNumber = getRandomInt(9);
+    const equationValue = firstNumber * secondNumber;
+    const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`;
+    equationObject = { value: equation, evaluated: "true" };
+    equationsArray.push(equationObject);
+  }
+  // Loop through, mess with the equation results, push to array
+  for (let i = 0; i < wrongEquations; i++) {
+    firstNumber = getRandomInt(9);
+    secondNumber = getRandomInt(9);
+    const equationValue = firstNumber * secondNumber;
+    wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
+    wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
+    wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
+    const formatChoice = getRandomInt(3);
+    const equation = wrongFormat[formatChoice];
+    equationObject = { value: equation, evaluated: "false" };
+    equationsArray.push(equationObject);
+  }
+  // radom equations
+  shuffle(equationsArray);
+  console.log("equations array: ", equationsArray);
+}
+
+// Dynamically adding correct/incorrect equations
+// function populateGamePage() {
+//   // Reset DOM, Set Blank Space Above
+//   itemContainer.textContent = '';
+//   // Spacer
+//   const topSpacer = document.createElement('div');
+//   topSpacer.classList.add('height-240');
+//   // Selected Item
+//   const selectedItem = document.createElement('div');
+//   selectedItem.classList.add('selected-item');
+//   // Append
+//   itemContainer.append(topSpacer, selectedItem);
+
+//   // Create Equations, Build Elements in DOM
+
+//   // Set Blank Space Below
+//   const bottomSpacer = document.createElement('div');
+//   bottomSpacer.classList.add('height-500');
+//   itemContainer.appendChild(bottomSpacer);
+// }
+
 // Displays 3, 2, 1 G0
 function countdownStart() {
   countdown.textContent = "3";
@@ -51,6 +112,7 @@ function showCountdown() {
   countdownPage.hidden = false;
   splashPage.hidden = true;
   countdownStart();
+  createEquations();
 }
 
 // Get the value from selected radio button
